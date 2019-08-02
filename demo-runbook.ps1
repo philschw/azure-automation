37ad7ea4c80a3fe64437afcdb061c72d9701797d
philschw <schwanderphilip@gmx.net>
2019-08-02 18:08:40 +0200

    Add files via upload

---
+++ demo-runbook.ps1
@@ -0,0 +1,13 @@
+﻿Write-Output 'Hello World'
+
+$username = Get-AutomationVariable -Name 'username'
+
+Write-Output $username
+
+$username = $username + ' Juhu'
+
+Write-Output $username
+
+Set-AutomationVariable -Name 'username' -Value $username
+
+Write-Output $username
